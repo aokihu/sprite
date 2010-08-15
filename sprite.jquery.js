1895dc80044f2b6dd1676555d5dd0e91e98ddf1f
Hu.Jianan <aokihu@gmail.com>
2010-08-15 14:08:57 +0800

    create sprite.jquery.js file

---
+++ sprite.jquery.js
@@ -0,0 +1,15 @@
+/*
+ * sprite.jquery.js
+ * Just another jQuery plug
+ * use to layout HTML like desktop application
+ * 
+ *  author: Hu. Jianan(aokihu)
+ *   email: aokihu@gmail.com
+ * version: $Id$
+*/
+
+(function($){
+	$.fn.sprite = function(){
+		
+	}// @end $.fn.sprite
+})(jQUery);
